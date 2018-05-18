1d5a6047abbdd907efc31a6e09a90401fe83fd69
Valentina Cobo <vcobop@unal.edu.co>
2018-05-18 09:41:45 -0500

    Primera version de datos del programa proteinortho que no se usar

---
+++ Datos/protein.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /scr/hercules1san/vcobop/Proteinortho/
+
+proteinortho5.pl -p=blastn+ /scr/hercules1san/vcobop/Proteinortho/bosc_candidates_new.fa /scr/hercules1san/vcobop/Proteinortho/ciin_candidates_new.fa /scr/hercules1san/vcobop/Proteinortho/cisa_candidates_new.fa /scr/hercules1san/vcobop/Proteinortho/clob_candidates_new.fa /scr/hercules1san/vcobop/Proteinortho/haro_candidates_new.fa /scr/hercules1san/vcobop/Proteinortho/odi_candidates_new.fa /scr/hercules1san/vcobop/Proteinortho/pevi_candidates_new.fa /scr/hercules1san/vcobop/Proteinortho/sato_candidates_new.fa
